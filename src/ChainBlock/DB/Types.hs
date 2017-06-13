8ddce751efdd32856a438a210d20d13ea579f308
Charles Crain <charles@blockapps.net>
2017-06-12 21:08:54 -0400

    began tables for RDB

--- src/ChainBlock/DB/Types.hs
+++ src/ChainBlock/DB/Types.hs
@@ -2,9 +2,12 @@
 
 module ChainBlock.DB.Types where
 
-import           Data.Text    (Text)
+import           Data.Text               (Text)
 import           GHC.Generics
 
+import           Data.Profunctor.Product (p2, p3, p4)
+import           Opaleye                 (Column, PGInt4, PGText, Table (Table),
+                                          optional, required, (.<), (.==))
 -----------------------------------------------------
 -- | User
 -----------------------------------------------------
@@ -14,6 +17,11 @@ data User =
         , uId  :: UserId
         }
   deriving (Show, Eq, Generic)
+
+userTable :: Table (Maybe (Column PGInt4), Column PGText)
+                     (Column PGInt4, Column PGText)
+userTable = Table "userTable" (p2 ( optional "id"
+                                   , required "name" ))
 
 newtype UserId = UserId {unUserId :: Text}
   deriving (Show, Eq, Ord, Generic)
@@ -32,6 +40,12 @@ data WebsiteCredentials =
                       }
   deriving (Show, Eq, Ord, Generic)
 
+credentialsTable :: Table (Column PGText, Column PGText, Column PGInt4)
+                          (Column PGText, Column PGText, Column PGInt4)
+credentialsTable = Table "credentialsTable" (p3 ( required "username"
+                                                , required "encrypted_pass"
+                                                , required "website_id" ))
+
 data Website =
   Website { webURL      :: WebsiteURL
           , websiteName :: Text
@@ -39,6 +53,13 @@ data Website =
           , userId      :: UserId
           }
   deriving (Show, Eq, Ord, Generic)
+
+websiteTable :: Table (Column PGInt4, Column PGText, Column PGText, Column PGInt4)
+                      (Column PGInt4, Column PGText, Column PGText, Column PGInt4)
+websiteTable = Table "credentialsTable" (p4 ( required "id"
+                                            , required "website_url"
+                                            , required "website_name"
+                                            , required "user_id" ))
 
 newtype WebsiteId = WebsiteId  {unWebsiteId :: Integer}
   deriving (Show, Eq, Ord, Generic)
